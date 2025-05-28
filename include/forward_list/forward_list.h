ca27aada0ba4e78ab310dced3ea9bc79a4dbb9b0
quasar-chunawala <quasar.chunawala@gmail.com>
2025-05-28 10:11:34 +0100

    add make_shared implementation

--- include/forward_list/forward_list.h
+++ include/forward_list/forward_list.h
@@ -37,7 +37,43 @@ namespace dev{
             : m_current_node_ptr{ptr}
             {}
 
+            // Pre-increment 
+            Iterator& operator++(){
+                m_current_node_ptr = m_current_node_ptr->next;
+                return *this;
+            }
 
+            // Post-increment
+            Iterator operator++(int){
+                auto temp = *this;
+                m_current_node_ptr = m_current_node_ptr->next;
+                return temp;
+            }
+
+            bool operator==(const Iterator& other) const{
+                return (m_current_node_ptr == other.m_current_node_ptr);
+            }
+
+            bool operator!=(const Iterator& other) const{
+                return !(*this == other);
+            }
+
+            // Pointer-like operations
+            U& operator*(){
+                return m_current_node_ptr->m_value;
+            }
+
+            const U& operator*() const{
+                return m_current_node_ptr->m_value;
+            }
+
+            U* operator->(){
+                return m_current_node_ptr->value;
+            }
+
+            const U* operator() const{
+                return m_current_node_ptr->value;
+            }
         };
 
         size_type size() const{
@@ -46,6 +82,44 @@ namespace dev{
 
         bool empty() const{
             return (!m_head);
+        }
+
+        iterator begin(){
+            return iterator(m_head);
+        }
+
+        const_iterator begin() const{
+            return const_iterator(m_head);
+        }
+
+        const_iterator cbegin() const{
+            return const_iterator(m_head);
+        }
+
+        iterator end(){
+            return iterator(nullptr);
+        }
+
+        const_iterator end() const{
+            return iterator(nullptr);
+        }
+
+        const_iterator cend() const{
+            return end();
+        }
+
+        /* Destroy the containers content */
+        void clear() noexcept{
+            for(auto p {m_head}; p!=nullptr;){
+                auto q = p->next;
+                delete p;
+                p = q;
+            }
+            m_size = 0;
+        }
+
+        ~forward_list(){
+            clear();
         }
 
         private:
@@ -64,5 +138,65 @@ namespace dev{
 
         ListNode* m_head{nullptr};
         size_type m_size{0};
+
+        public:
+        using iterator = Iterator<T>;
+        using const_iterator = Iterator<const T>;
+
+        /* Constructors */
+        forward_list() = default;
+
+        template<std::input_iterator It>
+        forward_list(It b, It e){
+            if(b == e) return;
+            ListNode* m_curr{nullptr};
+
+            try{
+                for(auto it{b};it!=e;++it){
+                    if(empty())
+                    {
+                        m_head = new ListNode(*it);
+                        ++m_size;
+                        m_curr = m_head;
+                    }
+                    else{
+                        m_curr->next = new ListNode(*it);
+                        ++m_size;
+                        m_curr = m_curr->next;
+                    }
+                }
+            }catch(...){
+                clear();
+                throw;
+            }
+        }
+
+        forward_list(const forward_list& other):
+        forward_list(other.begin(), other.end())
+        {}
+
+        forward_list(std::initializer_list<T> other)
+        :forward_list(other.begin(), other.end())
+        {}
+        
+        forward_list(forward_list&& other)
+        : m_head{std::exchange(other.m_head, nullptr)}
+        , m_size{std::exchange(other.m_size,0)}
+        {}
+
+        void swap(forward_list& other){
+            using std::swap;
+            swap(m_head, other.m_head);
+            swap(m_size, other.m_size);
+        }
+
+        forward_list& operator=(const forward_list& other){
+            forward_list(other).swap(*this);
+            return *this;
+        }
+
+        forward_list& operator=(forward_list&& other){
+            
+        }
     };
 }
--- include/shared_ptr/shared_ptr.h
+++ include/shared_ptr/shared_ptr.h
@@ -2,6 +2,59 @@
 #include<atomic>
 
 namespace dev{
+    struct control_block_base{
+        std::atomic<unsigned long long> m_ref_count{1};
+
+        void increment(){
+            ++m_ref_count;
+        }
+
+        auto decrement(){
+            return --m_ref_count;
+        }
+
+        auto use_count(){
+            return m_ref_count.load();
+        }
+
+        virtual void release_shared() = 0;
+    };
+
+    template<typename T>
+    struct control_block : control_block_base{
+        T* m_object_ptr;
+        explicit control_block(T* data)
+        : control_block_base{}
+        , m_object_ptr{data}
+        {}
+
+        void release_shared() override{
+            if(decrement()==0)
+            {
+                delete m_object_ptr;
+                delete this;
+            }
+        }
+    };
+
+    template<typename T>
+    struct control_block_with_storage : control_block_base{
+        T m_object;
+        template<typename... Args>
+        explicit control_block_with_storage(Args&&... args)
+        : control_block_base{}
+        , m_object{T(std::forward<Args>(args)...)}
+        {}
+
+        void release_shared() override{
+            if(decrement() == 0){
+                delete this;
+            }
+        }
+
+        T* get(){ return &m_object; }
+    };
+
     template<typename T>
     class shared_ptr{
         public:
@@ -9,12 +62,17 @@ namespace dev{
         /* Default constructor*/
         shared_ptr() = default;
 
+        shared_ptr(T* t, control_block_base* cb)
+        : m_raw_underlying_ptr{t}
+        , m_control_block_ptr{cb}
+        {}
+
         /* Parametrized constructor : Takes ownership of the pointee */
         shared_ptr(T* ptr) 
         : m_raw_underlying_ptr{ptr}
         {
             try{
-                m_ref_count_ptr = new std::atomic<unsigned long long>(1LL);
+                m_control_block_ptr = new control_block(ptr);
             }catch(...){
                 delete ptr;
                 throw;
@@ -24,23 +82,23 @@ namespace dev{
         /* Copy constructor : Implements shared co-ownership of the pointee semantics */
         shared_ptr(const shared_ptr& other)
         : m_raw_underlying_ptr{other.m_raw_underlying_ptr}
-        , m_ref_count_ptr{other.m_ref_count_ptr}
+        , m_control_block_ptr{other.m_control_block_ptr}
         {
-            if(m_ref_count_ptr)
-                ++(*m_ref_count_ptr);   //Atomic pre-increment
+            if(m_control_block_ptr)
+                m_control_block_ptr->increment();   //Atomic pre-increment
         }
 
         /* Move constructor : Represents the transfer of ownership */
         shared_ptr(shared_ptr&& other)
         : m_raw_underlying_ptr{ std::exchange(other.m_raw_underlying_ptr, nullptr)}
-        , m_ref_count_ptr{ std::exchange(other.m_ref_count_ptr, nullptr)}
+        , m_control_block_ptr{ std::exchange(other.m_control_block_ptr, nullptr)}
         {}
 
         /* Swap : Swap two shared_ptr objects member by member */
         void swap(shared_ptr& other){
             using std::swap;
             std::swap(m_raw_underlying_ptr, other.m_raw_underlying_ptr);
-            std::swap(m_ref_count_ptr, other.m_ref_count_ptr);
+            std::swap(m_control_block_ptr, other.m_control_block_ptr);
         }
 
         friend void swap(shared_ptr& lhs, shared_ptr& rhs){
@@ -62,18 +120,8 @@ namespace dev{
         }
 
         ~shared_ptr(){
-            if(m_ref_count_ptr){
-                unsigned long long expected = m_ref_count_ptr->load();
-                
-                // Decrement the reference count
-                while(expected > 0 
-                    && !m_ref_count_ptr->compare_exchange_weak(expected, expected - 1));
-
-                auto desired = expected - 1;
-                if(desired == 0){   // We are the last user of *m_ref_underlying_ptr
-                    delete m_raw_underlying_ptr;
-                    delete m_ref_count_ptr;
-                }
+            if(m_control_block_ptr){
+                m_control_block_ptr->release_shared();
             }
         }
 
@@ -112,8 +160,8 @@ namespace dev{
         }
 
         unsigned long long use_count() const noexcept{
-            if(m_ref_count_ptr)
-                return m_ref_count_ptr->load();
+            if(m_control_block_ptr)
+                return m_control_block_ptr->use_count();
             else
                 return 0;
         }
@@ -124,8 +172,19 @@ namespace dev{
                 shared_ptr(ptr).swap(*this);
         }
 
+        template<typename U, typename... Args>
+        friend shared_ptr<U> make_shared(Args&&... args);
+
         private:
         T* m_raw_underlying_ptr{nullptr};
-        std::atomic<unsigned long long>* m_ref_count_ptr{nullptr};
+        control_block_base* m_control_block_ptr{nullptr};
     };
+
+    template<typename T, typename... Args>
+    shared_ptr<T> make_shared(Args&&... args){
+        /* Perform a single heap memory allocation */
+        control_block_with_storage<T>* cb = new control_block_with_storage<T>(std::forward<Args>(args)...);
+        T* data = cb->get();
+        return shared_ptr<T>(data, cb);
+    }
 }
